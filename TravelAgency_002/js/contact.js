46a7abb82c1abdac079e670c7ed344c72d96bf32
KawaRoro <webmaster.benjamin@gmail.com>
2020-03-25 15:25:38 +0100

    Update Agency Travel

--- TravelAgency_002/js/contact.js
+++ TravelAgency_002/js/contact.js
@@ -1,14 +1,68 @@
 
+let contact = document.getElementById('contact');
 
-const wrapper = document.getElementById('wrapper');
+//alert(contact.children[0]);
+
+let h3 = document.getElementsByTagName('h3');
+h3[0].setAttribute("id", "titleH3");
+
+var elements = document.getElementsByTagName("form");
+elements[0].setAttribute("action", "#contact");
+
+var elements = document.getElementsByTagName("input");
+for (let i = 0; i < elements.length; i++) {
+    if(i == 0){
+        elements[i].setAttribute("id", "name");
+    }
+    if(i == 1){
+        elements[i].setAttribute("id", "email");
+    }
+    if(i == 2){
+        elements[i].setAttribute("id", "button-3");
+    }
+}
+
+var elements = document.getElementsByTagName("label");
+for (let i = 0; i < elements.length; i++) {
+    if(i == 0){
+        elements[i].setAttribute("id", "nameL");
+    }
+    if(i == 1){
+        elements[i].setAttribute("id", "emailL");
+    }
+}
+
+//'<p align="center" id="formPC"></p><p align="right" id="formPR"><small><span style="color:red">*</span>Champs obligatoires</small></p>';
+let para = document.createElement("p");
+para.setAttribute("id", "formPC");
+//let node = document.createTextNode("");
+contact.appendChild(para);
+
+let para2 = document.createElement("p");
+para2.setAttribute("id", "formPR");
+//let small = document.createElement("small");
+//let span = document.createElement("span");
+//span.setAttribute("style", "color:red");
+//span.style.color = "red";
+//let node = document.createTextNode('*Champs obligatoires');
+//contact.appendChild(node);
+contact.appendChild(para2);
+//small.appendChild(span);
+//para2.appendChild(small);
+
+
+//const wrapper = document.getElementById('wrapper');
 const titleH3 = document.getElementById('titleH3');
 
 const name = document.getElementById('name'); // input
 const nameL = document.getElementById('nameL'); // label
+nameL.innerHTML = 'Nom<span style="color:red">*</span>';
 const email = document.getElementById('email'); // input
 const emailL = document.getElementById('emailL'); // label
+emailL.innerHTML = 'Email<span style="color:red">*</span>';
 const formPC = document.getElementById('formPC'); // Paragraph
 const formPR = document.getElementById('formPR'); // Paragraph
+formPR.innerHTML = '<small><span style="color:red">*</span>Champs obligatoires</small></p>';
 const buttonContact = document.getElementById('button-3'); // button 
 
 buttonContact.style.visibility = "hidden"; // visible 
